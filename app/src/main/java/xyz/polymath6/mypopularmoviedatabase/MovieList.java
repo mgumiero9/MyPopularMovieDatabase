bdbd7e4e02c3ee09674aed47a5a0d3098abd0a66
mgumiero9 <mgumiero9@gmail.com>
2016-11-11 16:31:01 -0200

    building layouts

--- app/src/main/java/xyz/polymath6/mypopularmoviedatabase/MovieList.java
+++ app/src/main/java/xyz/polymath6/mypopularmoviedatabase/MovieList.java
@@ -3,13 +3,14 @@ package xyz.polymath6.mypopularmoviedatabase;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class MovieList extends AppCompatActivity {
+public class MovieList extends AppCompatActivity implements SwipeRefreshLayout.OnRefreshListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,5 +49,10 @@ public class MovieList extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onRefresh() {
+
     }
 }
